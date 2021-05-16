a6f8ea82553ce76511089afa9b83bb6ba45c63bd
nvtrung97 <nvtrung.97hcmus@gmail.com>
2021-05-16 18:56:24 +0700

    feat: crawl data

---
+++ pup/index.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+var request = require('request');
+(async () => {
+
+    request('https://drive.google.com/drive/folders/0B4fU5__VXkIkdGZzdk5qMWFSVjg', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          //  console.log(response);
+            let data = JSON.stringify(response);
+           data=data.split('\\').join('');
+            data = data.split(',')
+            let newdata = [];
+            for (let i = 0; i < data.length; i++) {
+                if ((data[i]).length >= 15 && data[i].includes('x5d') == false && data[i].includes('Trường') == false && !isNumeric(data[i]))
+                    newdata.push(data[i])
+            }
+            newdata = JSON.stringify(newdata).split('x22');
+            let finalData = [];
+            for (let i = 1; i < newdata.length; i++) {
+                if ((newdata[i]).length >= 15 && !isNumeric(newdata[i])&& (newdata[i]).length <= 100)
+                finalData.push(newdata[i])
+            }
+            console.log(finalData);
+        }
+        else {
+            console.log("Error " + response.statusCode)
+        }
+    })
+})()
+
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
