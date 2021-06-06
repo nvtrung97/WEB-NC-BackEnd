acee3eb8419c80c759a3087025e9b21079d07b56
nvtrung97 <nvtrung.97hcmus@gmail.com>
2021-06-06 22:30:52 +0700

    feat: add verify admin in api admin

--- middlewares/route.mdw.js
+++ middlewares/route.mdw.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     app.use('/api/v1/products', require('../routes/product.route'));
     app.use('/api/v1/users', require('../routes/user.route'));
     app.use('/api/v1/reviews', require('../routes/review.route'));
-    app.use('/api/v1/admin/categories', require('./auth.mdw').verifyToken/* cần check admin */, require('../routes/admin/category.admin.route'));
-    app.use('/api/v1/admin/products', require('./auth.mdw').verifyToken/* cần check admin */, require('../routes/admin/product.admin.route'));
-    app.use('/api/v1/admin/users', require('./auth.mdw').verifyToken/* cần check admin */, require('../routes/admin/user.admin.route'));
+    app.use('/api/v1/admin/categories', require('./auth.mdw').verifyToken, require('./auth.mdw').verifyRoleAdmin, require('../routes/admin/category.admin.route'));
+    app.use('/api/v1/admin/products', require('./auth.mdw').verifyToken, require('./auth.mdw').verifyRoleAdmin, require('../routes/admin/product.admin.route'));
+    app.use('/api/v1/admin/users', require('./auth.mdw').verifyToken, require('./auth.mdw').verifyRoleAdmin, require('../routes/admin/user.admin.route'));
 };
