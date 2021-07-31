1e94af750af0b906349b097f05bc043348be8b43
nvtrung97 <nvtrung.97hcmus@gmail.com>
2021-07-31 15:55:23 +0700

    fix: cors

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+console.log(process.env.NODE_ENV);
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
